328de510558beb86ed5e5658ad2145f99913cc32
1532468 <1532468@naver.com>
2018-10-12 13:55:49 +0900

    delete comment

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -8,28 +8,30 @@ def run():
     HOST = '192.168.0.90'
     PORT = 5034
     #socket create for camera
-    #camera_socket = server_connect.Connect(HOST, PORT)
+    camera_socket = server_connect.Connect(HOST, PORT)
+    
     #socket create for ultrasonic sensor
-    us_socket = server_connect.Connect(HOST, PORT)
+    #us_socket = server_connect.Connect(HOST, PORT+1)
+    
     #create raspberrypi object
     serial = rasp_serial.Serial()    
     
     #if connected to server..start thread and send camera frame
-    #camera = Camera.Camera(camera_socket.Get_Socket())
-    #camera_thread = threading.Thread(target=camera.run, args=())
-    #camera_thread.start()
+    camera = Camera.Camera(camera_socket.Get_Socket())
+    camera_thread = threading.Thread(target=camera.run, args=())
+    camera_thread.start()
 
-    us = ultrasonic.UltraSonic()    
-    us_thread = threading.Thread(target=us.run, args=(us_socket,))
-    us_thread.start()
+    #us = ultrasonic.UltraSonic()    
+    #us_thread = threading.Thread(target=us.run, args=(us_socket,))
+    #us_thread.start()
 
     print('start')
 
-    #while True:
-    #    data =  server.Get_Data()
-    #    if data == 'q' :
-    #        break
-    #    serial.steer(data)
+    while True:
+        data =  server.Get_Data()
+        if data == 'q' :
+            break
+        serial.steer(data)
     
 if __name__ == "__main__" :
     run()
