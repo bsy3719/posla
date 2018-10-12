6673bdacda799ce80d9887aa318b824b3af92c51
1532468 <1532468@naver.com>
2018-10-12 13:12:18 +0900

    modified send_data() -> Send_Data()

--- raspberrypi/ultrasonic.py
+++ raspberrypi/ultrasonic.py
@@ -66,7 +66,7 @@ class UltraSonic(object) :
 
             distance = round((pulse_end - pulse_start) * 17000, 2)
               
-            server.send_data(str(distance).encode())
+            server.Send_Data(str(distance).encode())
 
     def __del__(self) :
         gpio.cleanup()
