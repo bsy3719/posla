3f27cb6b7d2fb979f06489995635eb6892622268
1532468 <1532468@naver.com>
2018-10-12 17:26:22 +0900

    delete comment, add Send_Data()

--- raspberrypi/mic.py
+++ raspberrypi/mic.py
@@ -10,13 +10,7 @@ from google.cloud.speech import types
 import pyaudio
 from six.moves import queue
 
-# Audio recording parameters
-#RATE = 16000
-#CHUNK = int(RATE / 10)  # 100ms
-
-
 class MicrophoneStream(object):
-    """Opens a recording stream as a generator yielding the audio chunks."""
     def __init__(self):
         self.rate = 16000
         self.chunk = int(self.rate / 10)
@@ -44,27 +38,20 @@ class MicrophoneStream(object):
         self._audio_stream.stop_stream()
         self._audio_stream.close()
         self.closed = True
-        # Signal the generator to terminate so that the client's
-        # streaming_recognize method will not block the process termination.
         self._buff.put(None)
         self._audio_interface.terminate()
 
     def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
-        """Continuously collect data from the audio stream, into the buffer."""
         self._buff.put(in_data)
         return None, pyaudio.paContinue
 
     def generator(self):
         while not self.closed:
-            # Use a blocking get() to ensure there's at least one chunk of
-            # data, and stop iteration if the chunk is None, indicating the
-            # end of the audio stream.
             chunk = self._buff.get()
             if chunk is None:
                 return
             data = [chunk]
 
-            # Now consume whatever other data's still buffered.
             while True:
                 try:
                     chunk = self._buff.get(block=False)
@@ -77,7 +64,7 @@ class MicrophoneStream(object):
             yield b''.join(data)
 
 
-    def listen_print_loop(self, responses):
+    def listen_print_loop(self, responses, server):
         num_chars_printed = 0
         for response in responses:
             if not response.results:
@@ -99,51 +86,14 @@ class MicrophoneStream(object):
 
             else:
                 print('dddd', transcript + overwrite_chars)
-
-            # Exit recognition if any of the transcribed phrases could be
-            # one of our keywords.
-           # if re.search(r'\b(exit|quit)\b', transcript, re.I):
-            #    print('Exiting..')
-             #   break
+                server.Send_Data((transcript + overwrite_chars)).encode())
 
                 num_chars_printed = 0
 
-    def run(self) :
+    def run(self, server) :
         audio_generator = self.generator()
         requests = (types.StreamingRecognizeRequest(audio_content=content) for content in audio_generator)
         responses = self.client.streaming_recognize(self.streaming_config, requests)
-        self.listen_print_loop(responses)
+        self.listen_print_loop(responses, server)
 
 
-#def main():
-    # See http://g.co/cloud/speech/docs/languages
-    # for a list of supported languages.
-    #language_code = 'en-US'  # a BCP-47 language tag
-    #language_code = 'ko-KR'
-    
-    #import os
-
-    #os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="/home/pirl/Documents/BCODE/project/speech/Speech-58bc93fd5c7a.json"
-    
-    #client = speech.SpeechClient()
-    #config = types.RecognitionConfig(
-    #    encoding=enums.RecognitionConfig.AudioEncoding.LINEAR16,
-    #    sample_rate_hertz=RATE,
-    #    language_code=language_code)
-    #streaming_config = types.StreamingRecognitionConfig(
-    #    config=config,
-    #    interim_results=True)
-
-#    with MicrophoneStream(RATE, CHUNK) as stream:
-#        audio_generator = stream.generator()
-#        requests = (types.StreamingRecognizeRequest(audio_content=content)
-#                    for content in audio_generator)
-#
-#        responses = client.streaming_recognize(streaming_config, requests)
-#
-        # Now, put the transcription responses to use.
-#        listen_print_loop(responses)
-
-
-#if __name__ == '__main__':
-#    main()
