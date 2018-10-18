7869dd9585f7ed7e1520c17eeba88d4864699056
1532468 <1532468@naver.com>
2018-10-18 17:16:07 +0900

    delete comment

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -4,18 +4,21 @@ import Camera
 import threading
 import ultrasonic
 import mic
+import time
 
 def run():
-    HOST = '192.168.0.90'
-    PORT = 5036
+    HOST = '192.168.0.96'
+    PORT = 5034
     #socket create for camera
     server_socket = server_connect.Connect(HOST, PORT)
-    
+    time.sleep(1)
+
     #socket create for ultrasonic sensor
-    #us_socket = server_connect.Connect(HOST, PORT+1)
-    
+    us_socket = server_connect.Connect(HOST, PORT+1)
+    time.sleep(1)
+
     #socket create for mic
-    #mic_socket = server_connect.Connect(HOST, PORT+2)
+    mic_socket = server_connect.Connect(HOST, PORT+2)
 
     #create raspberrypi object
     serial = rasp_serial.Serial()    
@@ -25,13 +28,13 @@ def run():
     camera_thread = threading.Thread(target=camera.run, args=())
     camera_thread.start()
 
-    #us = ultrasonic.UltraSonic()    
-    #us_thread = threading.Thread(target=us.run, args=(us_socket,))
-    #us_thread.start()
+    us = ultrasonic.UltraSonic()    
+    us_thread = threading.Thread(target=us.run, args=(us_socket,))
+    us_thread.start()
 
-    #microphone = mic.MicrophoneStream()    
-    #microphone_thread = threading.Thread(target=microphone.run, args=(mic_socket,))
-    #microphone_thread.start()
+    microphone = mic.MicrophoneStream()    
+    microphone_thread = threading.Thread(target=microphone.run, args=(mic_socket,))
+    microphone_thread.start()
 
     print('start')
 
