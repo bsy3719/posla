cad7761ebb3d207909804cc9294d21b2c134219b
1532468 <1532468@naver.com>
2018-10-18 17:08:12 +0900

    add source file

---
+++ computer/Object.py
@@ -0,0 +1,28 @@
+from pydarknet import Detector, Image
+import cv2
+import threading
+import numpy as np
+
+import server_steer
+
+class Object_Detection():
+
+    def __init__(self):
+
+        self.net = Detector(bytes("YOLOv3/cfg/yolo-obj.cfg", encoding="utf-8"), 
+                   bytes("YOLOv3/cfg/yolo-obj_400.weights", encoding="utf-8"), 
+                   0, 
+                   bytes("YOLOv3/cfg/obj.data", encoding="utf-8"))
+        
+    def Dectection(self, img):  
+        cv2.imshow('dect', img)   
+        return self.net.detect(Image(img))       
+       
+'''
+        for cat, score, bounds in results:
+            x, y, w, h = bounds
+            cv2.rectangle(img, (int(x - w / 2), int(y - h / 2)), (int(x + w / 2), int(y + h / 2)), (255, 0, 0), thickness=2)
+            cv2.putText(img, str(cat.decode("utf-8")), (int(x - w / 2 - 1), int(y - h / 2 - 1)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+            print(cat)
+'''
+
