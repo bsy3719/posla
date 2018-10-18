d6245a4724af696a22f2c2913370debe91609e50
KKojimaru <lcs11244@naver.com>
2018-10-18 19:36:30 +0900

    added change code rgb2bgr scale

--- computer/StopLineDetector/StopLine.py
+++ computer/StopLineDetector/StopLine.py
@@ -33,11 +33,13 @@ class Stop():
         self.sum_cx2 -= y2
 
     def GetStopLine(self, img):
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
         edge = cv2.Canny(img, 200, 350)
         row, col, ch = img.shape
         # print(img.shape)
         lines = cv2.HoughLinesP(edge, 1, math.pi / 180.0, 50, np.array([]), 50, 80)
         cx1 = cy1 = cx2 = cy2 = 0
+        print(lines)
         if lines is not None:
             a, b, c = lines.shape
             (d, e) = (row / 2, col / 2)
@@ -67,11 +69,12 @@ class Stop():
             (cx1, cy1, cx2, cy2) = (self.sum_cx1 / self.filtercnt, self.sum_cy1 / self.filtercnt, self.sum_cx2 / self.filtercnt, self.sum_cy2 / self.filtercnt)
         # cv2.line(img, (int(cx1), int(cy1)), (int(cx2), int(cy2)), (0, 0, 255), 3, cv2.LINE_AA)
         # print("Detected Stop Line's angle is", angle(cx2 - cx1, cy2 - cy1))
-        cv2.imshow('Detected Line', img)
+        # cv2.imshow('Detected Line', img)
         if cx1 is not 0 and cy1 is not 0 and cx2 is not 0 and cy2 is not 0:
             return True
         else:
             return False
+
 
 # def main():
 #     cap = cv2.VideoCapture(0)
