d79870c7297828ef891d941e09923998430b3535
1532468 <1532468@naver.com>
2018-10-21 13:28:47 +0900

    modified for object

--- computer/server_video.py
+++ computer/server_video.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import threading
 
 import Object
 import StopLine
@@ -15,11 +14,11 @@ class CollectTrainingData(object):
         self.steer = steer
 
         self.stopline = StopLine.Stop()
-        self.dect = Object.Object_Detection()
+        self.dect = Object.Object_Detection(self.steer)
 
         # model create
         self.model = NeuralNetwork()
-        self.model.load_model(path = 'training_data/video_model_1.h5')          
+        self.model.load_model(path = 'model_data/video_model_1.h5')          
 
     def collect(self):
 
@@ -33,33 +32,34 @@ class CollectTrainingData(object):
             last = stream_bytes.find(b'\xff\xd9')
 
             if first != -1 and last != -1:
-                jpg = stream_bytes[first:last + 2]
-                stream_bytes = stream_bytes[last + 2:]
+                try:
+                    jpg = stream_bytes[first:last + 2]
+                    stream_bytes = stream_bytes[last + 2:]
 
-                image = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
-                rgb = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)   
-                roi = image[120:240, :]
+                    image = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+                    rgb = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                    rgb2 = cv2.imdecode(np.frombuffer(jpg, dtype=np.uint8), cv2.IMREAD_COLOR)
+                    roi = image[120:240, :]
+                    roi2 = rgb2[120:240, :] #for line roi
 
                 
-                cv2.imshow('RGB', rgb)
-                cv2.imshow('GRAY', image)
-                cv2.imshow('roi', roi)
+                    cv2.imshow('RGB', rgb)
+                    cv2.imshow('GRAY', image)
+                    cv2.imshow('roi', roi)
 
-                # reshape the roi image into a vector
-                image_array = np.reshape(roi, (-1, 120, 320, 1))                  
+                    # reshape the roi image into a vector
+                    image_array = np.reshape(roi, (-1, 120, 320, 1))                  
 
-                try:
                     # neural network makes prediction
                     self.steer.Set_Line(self.model.predict(image_array))
-                    print(self.stopline.GetStopLine(rgb))
-                    self.steer.Set_ObjectDectection(self.dect.Dectection(rgb))                    
+                    self.steer.Set_Stopline(self.stopline.GetStopLine(roi2))
+                    self.dect.Detection(rgb)                    
                     self.steer.Control()
                 except:
                     continue
 
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
-
 
 
 
