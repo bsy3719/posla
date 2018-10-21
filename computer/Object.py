d39d973a73cfbfecfdbeeaca2d43f0bf4a7f9c0c
1532468 <1532468@naver.com>
2018-10-21 13:26:42 +0900

    show image and return list

--- computer/Object.py
+++ computer/Object.py
@@ -7,22 +7,33 @@ import server_steer
 
 class Object_Detection():
 
-    def __init__(self):
+    def __init__(self, steer):
 
+        self.steer = steer
         self.net = Detector(bytes("YOLOv3/cfg/yolo-obj.cfg", encoding="utf-8"), 
                    bytes("YOLOv3/cfg/yolo-obj_400.weights", encoding="utf-8"), 
                    0, 
                    bytes("YOLOv3/cfg/obj.data", encoding="utf-8"))
         
-    def Dectection(self, img):  
-        cv2.imshow('dect', img)   
-        return self.net.detect(Image(img))       
+    def Detection(self, img):  
+  
+        results = self.net.detect(Image(img))       
        
-'''
+        detect_list = []
+
         for cat, score, bounds in results:
             x, y, w, h = bounds
-            cv2.rectangle(img, (int(x - w / 2), int(y - h / 2)), (int(x + w / 2), int(y + h / 2)), (255, 0, 0), thickness=2)
-            cv2.putText(img, str(cat.decode("utf-8")), (int(x - w / 2 - 1), int(y - h / 2 - 1)), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
-            print(cat)
-'''
+            cv2.rectangle(img, 
+                          (int(x - w / 2), int(y - h / 2)), 
+                          (int(x + w / 2), int(y + h / 2)), 
+                          (255, 0, 0), 
+                          thickness=2)
+            cv2.putText(img, 
+                        str(cat.decode("utf-8")), 
+                        (int(x - w / 2 - 1), int(y - h / 2 - 1)), 
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0))
+            detect_list.append(cat.decode())
 
+        cv2.imshow('dect', img)
+
+        self.steer.Set_ObjectDetection(detect_list)
